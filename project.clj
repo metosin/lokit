e6d84413f8674cc2cf4cc988d68ef2d2f0c6acf3
Juho Teperi <juho.teperi@iki.fi>
2015-10-18 16:22:48 +0300

    Move clojure dependency to dev deps

--- project.clj
+++ project.clj
@@ -5,10 +5,9 @@
             :url          "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments     "same as Clojure"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.3.1"]
+  :dependencies [[org.clojure/tools.logging "0.3.1"]
                  [org.slf4j/jcl-over-slf4j "1.7.12"]
                  [org.slf4j/jul-to-slf4j "1.7.12"]
                  [org.slf4j/log4j-over-slf4j "1.7.12"]
-                 [ch.qos.logback/logback-classic "1.1.3"
-                  :exclusions [org.slf4j/slf4j-api]]])
+                 [ch.qos.logback/logback-classic "1.1.3" :exclusions [org.slf4j/slf4j-api]]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}})
